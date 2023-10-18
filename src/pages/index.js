4c64c04448e8c5b57cb44ba7549bce68718f7b3f
souravjanaomniful <souravjana13196@gmail.com>
2023-10-18 13:22:45 +0530

    modified

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,56 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useRouter } from "next/router";
-import Penguin from '../../public/penguin.png'
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const router = useRouter();
   return (
-    <main className="p-2">
-      <div className="p-2">
-        <h1 className="text-3xl font-semibold text-center dark:text-white">
-          Penguin AI
-        </h1>
-        
-      </div>
-      <div>
-        <div className="mt-6 my-2 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:p-10 xl:gap-x-8">
-          {/* <div
+    <>
+      <Head>
+        <title>Penguin AI</title>
+        <meta name="description" content="Penguin" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,700&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <main>
+        <nav className="dark:bg-gray-900">
+          <div className="flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="#" className="flex items-center">
+              <img
+                src="/happy-penguin.png"
+                className="h-16 mr-8 md:ml-8"
+                alt="Flowbite Logo"
+              />
+              <span className="self-center text-4xl font-semibold whitespace-nowrap text-white dark:text-white">
+                Penguin AI
+              </span>
+            </a>
+
+            <div
+              className="hidden w-full md:block md:w-auto"
+              id="navbar-default"
+            >
+              <img
+                src="https://www.vizuara.com/Vizlogo.gif"
+                className="h-12 md:mr-8"
+                alt="Flowbite Logo"
+              />
+            </div>
+          </div>
+        </nav>
+
+        <div>
+          <div className="mt-20 my-2 mx-4 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:p-10 xl:gap-x-8">
+            {/* <div
             className="group relative shadow-md rounded-md bg-slate-200"
             onClick={() => {
               router.push("/lessonplan");
@@ -31,7 +64,7 @@ export default function Home() {
               />
             </div>
             <div className="mt-4 px-4 pb-4">
-              <h3 className="text-2xl font-semibold text-center text-gray-700">
+              <h3 className="text-2xl font-semibold text-center text-black">
                 Ask a question
               </h3>
               <p className="mt-2 text-base text-black">
@@ -39,97 +72,142 @@ export default function Home() {
               </p>
             </div>
           </div> */}
-          <div
-            className="group relative shadow-md rounded-md bg-slate-200"
-            onClick={() => {
-              router.push("/lessonplan");
-            }}
-          >
-            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img
-                src="https://t3.ftcdn.net/jpg/05/34/31/48/360_F_534314856_Wo4ap1TJ2nb72tcoENHEVJ8aJ2CAzP9Y.jpg"
-                alt="Lesson Plans"
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-              />
+            <div
+              className="group relative shadow-md rounded-md bg-slate-200"
+              onClick={() => {
+                router.push("/lessonplan");
+              }}
+            >
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <img
+                  src="https://t3.ftcdn.net/jpg/05/34/31/48/360_F_534314856_Wo4ap1TJ2nb72tcoENHEVJ8aJ2CAzP9Y.jpg"
+                  alt="Lesson Plans"
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
+              <div className="mt-4 px-4 pb-4">
+                <h3 className="text-2xl font-semibold text-center text-black">
+                  Lesson Plans
+                </h3>
+                <p className="mt-2 text-base text-black text-center">
+                  Search about the best way to present a particular topic
+                </p>
+              </div>
             </div>
-            <div className="mt-4 px-4 pb-4">
-              <h3 className="text-2xl font-semibold text-center text-gray-700">
-                Lesson Plans
-              </h3>
-              <p className="mt-2 text-base text-black text-center">
-                Search about the best way to present a particular topic
-              </p>
+            <div
+              className="group relative shadow-md rounded-md bg-slate-200"
+              onClick={() => {
+                router.push("/lessonplan");
+              }}
+            >
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <img
+                  src="https://cdn.pixabay.com/photo/2015/10/31/12/00/question-1015308_1280.jpg"
+                  alt="Lesson Plans"
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
+              <div className="mt-4 px-4 pb-4">
+                <h3 className="text-2xl font-semibold text-center text-black">
+                  Doubt Clearance
+                </h3>
+                <p className="mt-2 text-base text-black text-center">
+                  If You Have Any Doubts, Ask Any Doubt
+                </p>
+              </div>
             </div>
-          </div>
-          <div
-            className="group relative shadow-md rounded-md bg-slate-200"
-            onClick={() => {
-              router.push("/lessonplan");
-            }}
-          >
-            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img
-                src="https://cdn.pixabay.com/photo/2015/10/31/12/00/question-1015308_1280.jpg"
-                alt="Lesson Plans"
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-              />
+            <div
+              className="group relative shadow-md rounded-md bg-slate-200"
+              onClick={() => {
+                router.push("/lessonplan");
+              }}
+            >
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <img
+                  src="https://site.surveysparrow.com/wp-content/uploads/2022/03/Copy-of-Blog-Banner-Templates-23-768x410.png"
+                  alt="Lesson Plans"
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
+              <div className="mt-4 px-4 pb-4">
+                <h3 className="text-2xl font-semibold text-center text-black">
+                  Multiple Choice Questions
+                </h3>
+                <p className="mt-2 text-base text-black text-center">
+                  Create Questions with multiple choices of answers
+                </p>
+              </div>
             </div>
-            <div className="mt-4 px-4 pb-4">
-              <h3 className="text-2xl font-semibold text-center text-gray-700">
-                Doubt Clearance
-              </h3>
-              <p className="mt-2 text-base text-black text-center">
-                If You Have Any Doubts, Ask Any Doubt
-              </p>
-            </div>
-          </div>
-          <div
-            className="group relative shadow-md rounded-md bg-slate-200"
-            onClick={() => {
-              router.push("/lessonplan");
-            }}
-          >
-            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img
-                src="https://site.surveysparrow.com/wp-content/uploads/2022/03/Copy-of-Blog-Banner-Templates-23-768x410.png"
-                alt="Lesson Plans"
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-              />
-            </div>
-            <div className="mt-4 px-4 pb-4">
-              <h3 className="text-2xl font-semibold text-center text-gray-700">
-                Multiple Choice Questions
-              </h3>
-              <p className="mt-2 text-base text-black text-center">
-                Create Questions with multiple choices of answers
-              </p>
-            </div>
-          </div>
 
-          <div
-            className="group relative shadow-md rounded-md bg-slate-200"
-            onClick={() => {
-              router.push("/lessonplan");
-            }}
-          >
-            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img
-                src="https://images.unsplash.com/photo-1455390582262-044cdead277a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZXNzYXl8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
-                alt="Lesson Plans"
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-              />
+            <div
+              className="group relative shadow-md rounded-md bg-slate-200"
+              onClick={() => {
+                router.push("/lessonplan");
+              }}
+            >
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <img
+                  src="https://images.unsplash.com/photo-1455390582262-044cdead277a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZXNzYXl8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+                  alt="Lesson Plans"
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
+              <div className="mt-4 px-4 pb-4">
+                <h3 className="text-2xl font-semibold text-center text-black">
+                  Essay
+                </h3>
+                <p className="mt-2 text-base text-black text-center">
+                  Create a well-structured essay
+                </p>
+              </div>
             </div>
-            <div className="mt-4 px-4 pb-4">
-              <h3 className="text-2xl font-semibold text-center text-gray-700">
-                Essay
-              </h3>
-              <p className="mt-2 text-base text-black text-center">
-                Create a well-structured essay
-              </p>
+            <div
+              className="group relative shadow-md rounded-md bg-slate-200"
+              onClick={() => {
+                router.push("/lessonplan");
+              }}
+            >
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <img
+                  src="https://images.unsplash.com/photo-1533271802434-53997a5f9e6c?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2070"
+                  alt="Lesson Plans"
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
+              <div className="mt-4 px-4 pb-4">
+                <h3 className="text-2xl font-semibold text-center text-black">
+                  Poetry
+                </h3>
+                <p className="mt-2 text-base text-black text-center">
+                  A Poetry Writing Adventure
+                </p>
+              </div>
+            </div>
+            <div
+              className="group relative shadow-md rounded-md bg-slate-200"
+              onClick={() => {
+                router.push("/lessonplan");
+              }}
+            >
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <img
+                  src="https://images.unsplash.com/photo-1462331940025-496dfbfc7564?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1822"
+                  alt="Lesson Plans"
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
+              <div className="mt-4 px-4 pb-4">
+                <h3 className="text-2xl font-semibold text-center text-black">
+                  Interesting Facts
+                </h3>
+                <p className="mt-2 text-base text-black text-center">
+                  Search about interesting facts
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
--- src/pages/lessonplan/index.js
+++ src/pages/lessonplan/index.js
@@ -34,6 +34,7 @@ const LessonPlan = () => {
   const [inputData, setInputData] = useState({
     topic: "",
   });
+  const [isLoading, setIsLoading] = useState(false);
   const [resposeMessage, setResponseMessage] = useState(null);
   const [isCopied, setCopied] = useClipboard(resposeMessage, {
     successDuration: 3000,
@@ -58,10 +59,10 @@ const LessonPlan = () => {
   };
   const handleChat = async (payload) => {
     const openai = new OpenAI({
-      apiKey: "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",
+      apiKey: process.env.NEXT_PUBLIC_CHATGPT_API_KEY,
       dangerouslyAllowBrowser: true,
     });
-
+    setIsLoading(true);
     try {
       const chatCompletion = await openai.chat.completions.create({
         messages: [
@@ -76,36 +77,39 @@ const LessonPlan = () => {
         ],
         model: "gpt-3.5-turbo",
       });
+      setIsLoading(false);
 
-      const response = chatCompletion.choices[0].message.content;
+      const response = chatCompletion?.choices[0]?.message?.content;
 
       setResponseMessage(response);
     } catch (error) {
       console.error(error);
       // Handle errors here
+      setIsLoading(false);
     }
   };
-  const getResponse = async (payload) => {
-    try {
-      const response = await fetch(
-        `http://localhost:3000/api/lessonplan/generatetext`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(payload),
-        }
-      );
-      const data = await response.json();
-      //   console.log("Data", data);
-      setResponseMessage(data.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+//   const getResponse = async (payload) => {
+//     try {
+//       const response = await fetch(
+//         `http://localhost:3000/api/lessonplan/generatetext`,
+//         {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify(payload),
+//         }
+//       );
+//       const data = await response.json();
+//       //   console.log("Data", data);
+//       setResponseMessage(data.data);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
   const handleSubmitData = (e) => {
     e.preventDefault();
+    SpeechRecognition.stopListening();
     handleChat(inputData.topic);
     // getResponse(inputData.topic);
   };
@@ -115,19 +119,19 @@ const LessonPlan = () => {
   //       document.execCommand("copy");
   //     }
   //   };
-  const postAudioFile = async (formData) => {
-    try {
-      const response = await fetch(`http://localhost:8080/transcript/audio`, {
-        method: "POST",
-        body: formData,
-      });
-      const data = await response.json();
-      //   console.log("Success", data);
-      setInputData({ ...inputData, topic: data.data });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  //   const postAudioFile = async (formData) => {
+  //     try {
+  //       const response = await fetch(`http://localhost:8080/transcript/audio`, {
+  //         method: "POST",
+  //         body: formData,
+  //       });
+  //       const data = await response.json();
+  //       //   console.log("Success", data);
+  //       setInputData({ ...inputData, topic: data.data });
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
 
   //   const handleStop = async (blobUrl, blob) => {
   //     // console.log("Blob data", blob);
@@ -141,7 +145,7 @@ const LessonPlan = () => {
   }, [transcript]);
   return (
     <div className="p-10">
-      <div className="m-auto w-[600px] justify-center items-center text-center">
+      <div className="m-auto justify-center items-center text-center">
         <h2 className="text-white font-semibold">Speak</h2>
 
         <div className="mt-2">
@@ -189,7 +193,7 @@ const LessonPlan = () => {
           )}
         />
       </div> */}
-      <form className="w-[600px] m-auto" onSubmit={handleSubmitData}>
+      <form className="m-auto max-w-5xl" onSubmit={handleSubmitData}>
         <label for="chat" className="sr-only">
           Your message
         </label>
@@ -198,7 +202,7 @@ const LessonPlan = () => {
             id="chat"
             rows="1"
             className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Topic"
+            placeholder="Send a Message"
             name="topic"
             value={inputData?.topic}
             onChange={handleInputStateChange}
@@ -208,62 +212,84 @@ const LessonPlan = () => {
             type="submit"
             className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600"
           >
-            <svg
-              className="w-5 h-5 rotate-90"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 18 20"
-            >
-              <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
-            </svg>
-          </button>
-        </div>
-      </form>
-
-      <div className="w-[600px] m-auto mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-        <div className="flex items-center justify-between px-3 py-2 border-b dark:border-gray-600">
-          <p>Response</p>
-          <button
-            type="button"
-            data-tooltip-target="tooltip-fullscreen"
-            className="p-2 text-gray-500 rounded cursor-pointer sm:ml-auto hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
-            // onClick={handleCopyToClipboard}
-            onClick={setCopied}
-          >
-            {isCopied ? (
-              "Copied"
+            {isLoading ? (
+              <svg
+                aria-hidden="true"
+                role="status"
+                class="inline w-5 h-5 text-gray-200 animate-spin dark:text-gray-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                ></path>
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="#1C64F2"
+                ></path>
+              </svg>
             ) : (
               <svg
-                className="h-6 w-6"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                className="w-5 h-5 rotate-90"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 18 20"
               >
-                <path stroke="none" d="M0 0h24v24H0z" />{" "}
-                <rect x="8" y="8" width="12" height="12" rx="2" />{" "}
-                <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" />
+                <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
               </svg>
             )}
           </button>
         </div>
-        <div className="px-4 py-2 bg-white rounded-b-lg dark:bg-gray-800">
-          <textarea
-            id="editor"
-            ref={textAreaRef}
-            rows="8"
-            className="block h-[400px] w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
-            placeholder=""
-            value={resposeMessage}
-            required
-          ></textarea>
+      </form>
+      {resposeMessage && (
+        <div className="m-auto max-w-5xl mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+          <div className="flex items-center justify-between px-3 py-2 border-b dark:border-gray-600">
+            <p className="font-semibold">Response</p>
+            <button
+              type="button"
+              data-tooltip-target="tooltip-fullscreen"
+              className="p-2 text-gray-500 rounded cursor-pointer sm:ml-auto hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+              // onClick={handleCopyToClipboard}
+              onClick={setCopied}
+            >
+              {isCopied ? (
+                "Copied"
+              ) : (
+                <svg
+                  className="h-6 w-6"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" />{" "}
+                  <rect x="8" y="8" width="12" height="12" rx="2" />{" "}
+                  <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" />
+                </svg>
+              )}
+            </button>
+          </div>
+
+          <div className="px-4 py-2 bg-white rounded-b-lg dark:bg-gray-800">
+            <textarea
+              id="editor"
+              ref={textAreaRef}
+              rows="8"
+              className="block h-[400px] w-full px-0 text-xl text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+              placeholder=""
+              value={resposeMessage}
+              required
+            ></textarea>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
